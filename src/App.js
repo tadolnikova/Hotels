707b7021db5d58e048b8093bdd3e9f07d53461bd
Татьяна <tatyana@iMac-Tatana.local>
2022-11-10 21:53:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from "./data";
 import './App.css';
 
 function App() {
+  const [ hotels, setHotels] = useState (data);
+  const [showText, setShowText] = useState(false);
+  const removeHotel = (id)=>{
+    //console.log (id);
+    let newHotels = hotels.filter((hotel)=> hotel.id !==id);
+    setHotels (newHotels)
+    
+  }
+  const showTextClick = (item) => {
+    item
+    .showMore = ! item.showMore
+    setShowText (! showText)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className ='container'>
+      <h1> NYC TOP {hotels.length} HOTELS</h1>
     </div>
-  );
+    {hotels.map ((item => {
+      const {id, hotelName, description, image, source, showMore} = item;
+      return(
+        <div  key={id}>
+        <div className = 'container'>
+          <h2>{id} - {hotelName}</h2>
+          </div>
+          <div className = 'container'>
+            <p>{showMore ? description: description.substring (0,220)+ "...."}
+            <button onClick = {() =>showTextClick (item)}>{showMore ? "show less" : "show more"}</button>
+            </p>
+            </div>
+            <div className='container'>
+              <img src ={image} width="500px" alt="NYC"/>
+            </div>
+            <div className='container'>
+              <p> {source}</p>
+            </div>
+      
+            <div className = 'container'>
+            <button className="btn" onClick={() =>removeHotel(id)}>Remove</button>
+            </div>
+        </div>)
+    }))}
+    
+    </div>
+  )
 }
 
 export default App;
